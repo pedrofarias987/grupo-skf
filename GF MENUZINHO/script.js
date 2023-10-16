dfdb3f567c929ffe2d1d2d2f75e716e79bed601a
Gabriel <142261891+Gabziell@users.noreply.github.com>
2023-10-16 16:58:09 -0300

    Add files via upload

---
+++ GF MENUZINHO/script.js
@@ -0,0 +1,8 @@
+
+const meuBotao = document.getElementById("meuBotao")
+function redirecionar() {
+
+ window.location.href = "https://uudt-ma.github.io/img/regra/regra5.jpg"
+}
+
+meuBotao.addEventListener("click", redirecionar)
